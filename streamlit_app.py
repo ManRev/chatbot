f3a391878ec263925d1625a7e3df59d5979c3d60
ManRev <mrevel@mit.edu>
2024-01-21 16:37:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ st.set_page_config(page_title="🤗💬 HugChat")
 # Hugging Face Credentials
 with st.sidebar:
     sign = Login(st.secrets["EMAIL"], st.secrets["PASS"])
+    hf_email = st.secrets['EMAIL']
+    hf_pass = st.secrets['PASS']
     #cookies = sign.login()
     #sign.saveCookies()
     st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-an-llm-powered-chatbot-with-streamlit/)!')
